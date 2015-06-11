93c7051ed126db77fa5f1e6c882746eba047159b
Prashant Kumar <prashant.kr121191@gmail.com>
2015-06-12 02:57:42 +0530

    fix indentation

--- CodeIgniter-3.0.0/application/controllers/Crawler.php
+++ CodeIgniter-3.0.0/application/controllers/Crawler.php
@@ -5,59 +5,59 @@ class Crawler extends CI_Controller {
 
 	public function index()
 	{
-        //Load webpage which takes input
+		//Load webpage which takes input
 		$this->load->view('Query');
 	}
 
-    //'Query' view will send input data back to this controller
-    //This function will send input to the c++ program 'crawl_server' 
-    //and receive the corresponding schedule.
-    //Finally it'll load another view named 'result' which will display response to 
-    //the user.
+	//'Query' view will send input data back to this controller
+	//This function will send input to the c++ program 'crawl_server' 
+	//and receive the corresponding schedule.
+	//Finally it'll load another view named 'result' which will display response to 
+	//the user.
 	public function process(){
-        //capturing input
+		//capturing input
 		$source = $this->input->post('src');
 		$destination = $this->input->post('dest');
 
 		//Getting Json output from the c++ program using sockets.
 		error_reporting(E_ALL);
-        $port = 4001;
-        $address = "localhost";
+		$port = 4001;
+		$address = "localhost";
 
-        $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-        if ($socket === false){
-            echo "Failed: ". socket_strerror(socket_last_error($socket))."\n";
-        }
+		$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+		if ($socket === false){
+			echo "Failed: ". socket_strerror(socket_last_error($socket))."\n";
+		}
 
-        // echo "Attempting to connect to '$address' on port '$port'...'\n";
-        $result = socket_connect($socket, $address, $port);
+		// echo "Attempting to connect to '$address' on port '$port'...'\n";
+		$result = socket_connect($socket, $address, $port);
 
-        if ($result === false){
-            echo "Failed: ". socket_strerror(socket_last_error($socket))."\n";
-        }
+		if ($result === false){
+			echo "Failed: ". socket_strerror(socket_last_error($socket))."\n";
+		}
 
-        //request data to be sent to the server
-        $request = $source . " " . $destination . " ";
-        $req = $request;
-        $response = '';
+		//request data to be sent to the server
+		$request = $source . " " . $destination . " ";
+		$req = $request;
+		$response = '';
 
-        // echo "Sending...\n";  
-        socket_send($socket, $req, strlen($req), MSG_WAITALL);
-        // echo "OK.\n";
+		// echo "Sending...\n";  
+		socket_send($socket, $req, strlen($req), MSG_WAITALL);
+		// echo "OK.\n";
 
-        // echo "Reading response:\n\n";
-        while ($out = socket_read($socket, 2048)) {
-        	if(is_null($out))
-            	break;
-            $response = $response . $out;
-        }
+		// echo "Reading response:\n\n";
+		while ($out = socket_read($socket, 2048)) {
+			if(is_null($out))
+				break;
+			$response = $response . $out;
+		}
 
-        //$data contains final response.
-        $data['res'] = $response;
+		//$data contains final response.
+		$data['res'] = $response;
 
-        socket_close($socket);
+		socket_close($socket);
 
-        //loading 'result' view and send $data to it.
+		//loading 'result' view and send $data to it.
 		$this->load->view('result', $data);
 
 	}
